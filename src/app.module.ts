badfde934ed3c8a2f81b79cd621d1ee73af65c0e
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-13 12:12:21 -0300

    feat: add and configure server static files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Connection, getConnectionOptions } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { storageConfig } from './config/storage';
 import { SharedModule } from './shared/shared.module';
 import { UsersModule } from './users/users.module';
 
@@ -14,6 +16,10 @@ import { UsersModule } from './users/users.module';
         Object.assign(await getConnectionOptions(), {
           autoLoadEntities: true,
         }),
+    }),
+    ServeStaticModule.forRoot({
+      rootPath: storageConfig.uploadFolder,
+      serveRoot: '/files',
     }),
     UsersModule,
     AuthModule,
