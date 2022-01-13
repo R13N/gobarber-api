4bbe6521acaf648ee9b8b1c83a61d970e8c96505
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-13 12:08:23 -0300

    feat: add serializer in routes

--- src/users/controllers/profile.controller.ts
+++ src/users/controllers/profile.controller.ts
@@ -1,20 +1,33 @@
-import { Body, Controller, Get, Put, Req } from '@nestjs/common';
+import {
+  Body,
+  ClassSerializerInterceptor,
+  Controller,
+  Get,
+  Put,
+  Req,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { UpdateProfileDTO } from '../dto/update-profile.dto';
 import { ShowProfileService } from '../services/show-profile.service';
 import { UpdateProfileService } from '../services/update-profile.service';
 
 @Controller('profile')
+@UseInterceptors(ClassSerializerInterceptor)
 export class ProfileController {
   constructor(
     private showProfileService: ShowProfileService,
     private updateProfileService: UpdateProfileService,
   ) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
-  create(@Req() req) {
+  get(@Req() req) {
     return this.showProfileService.execute(req.user.id);
   }
 
+  @UseGuards(JwtAuthGuard)
   @Put()
   update(
     @Req() req,
--- src/users/controllers/users.controller.ts
+++ src/users/controllers/users.controller.ts
@@ -1,26 +1,40 @@
 import {
   Body,
+  ClassSerializerInterceptor,
   Controller,
+  Patch,
   Post,
+  Req,
   UploadedFile,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateUserDTO } from '../dto/create-user.dto';
 import { CreateUserService } from '../services/create-user.service';
+import { UpdateUserAvatarService } from '../services/update-user-avatar.service';
 
 @Controller('users')
+@UseInterceptors(ClassSerializerInterceptor)
 export class UsersController {
-  constructor(private createUserService: CreateUserService) {}
+  constructor(
+    private createUserService: CreateUserService,
+    private updateUserAvatarService: UpdateUserAvatarService,
+  ) {}
 
   @Post()
-  create(@Body() createUserDTO: CreateUserDTO) {
+  async create(@Body() createUserDTO: CreateUserDTO) {
     return this.createUserService.execute(createUserDTO);
   }
 
-  @Post('avatar')
+  @UseGuards(JwtAuthGuard)
+  @Patch('avatar')
   @UseInterceptors(FileInterceptor('avatar'))
-  uploadFile(@UploadedFile() file: any) {
-    console.log(file);
+  uploadFile(@UploadedFile() file: Express.Multer.File, @Req() req) {
+    return this.updateUserAvatarService.execute({
+      avatarFilename: file.filename,
+      user_id: req.user.id,
+    });
   }
 }
