d55fd48a6a2bf10e32057cd17a2ecd67cb179abf
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-13 19:16:32 -0300

    feat: list providers  service

---
+++ src/modules/appointments/services/list-providers.service.ts
@@ -0,0 +1,18 @@
+import { User } from 'src/modules/users/entities/user.entity';
+import { UsersRepository } from 'src/modules/users/users.repository';
+
+interface IRequest {
+  user_id: string;
+}
+
+export class ListProvidersService {
+  constructor(private usersRepository: UsersRepository) {}
+
+  public async execute({ user_id }: IRequest): Promise<User[]> {
+    const users = await this.usersRepository.findAllProviders({
+      except_user_id: user_id,
+    });
+
+    return users;
+  }
+}
