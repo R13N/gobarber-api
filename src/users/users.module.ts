23fe1cfa5a2a26cd6c36d66a006c3dd1e20034f1
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-13 12:07:07 -0300

    feat: import ProfileControler in UserModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,12 +3,13 @@ import { MulterModule } from '@nestjs/platform-express';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { storageConfig } from 'src/config/storage';
 import { SharedModule } from 'src/shared/shared.module';
+import { ProfileController } from './controllers/profile.controller';
+import { UsersController } from './controllers/users.controller';
 import { CreateUserService } from './services/create-user.service';
 import { FindUserByEmailService } from './services/find-user-by-email.service';
 import { ShowProfileService } from './services/show-profile.service';
 import { UpdateProfileService } from './services/update-profile.service';
 import { UpdateUserAvatarService } from './services/update-user-avatar.service';
-import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 
 @Module({
@@ -21,7 +22,7 @@ import { UsersRepository } from './users.repository';
       }),
     }),
   ],
-  controllers: [UsersController],
+  controllers: [UsersController, ProfileController],
   providers: [
     FindUserByEmailService,
     CreateUserService,
