62524582cf43f832c12161f546d85f8c6b72e0bf
Saymon Damásio <saymon.damasio95@gmail.com>
2022-01-13 19:19:40 -0300

    feat: create appointments controller

--- src/modules/appointments/controllers/appointments.controller.ts
+++ src/modules/appointments/controllers/appointments.controller.ts
@@ -1,15 +1,17 @@
-import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from 'src/modules/auth/jwt-auth.guard';
 import { CreateAppointmentDTO } from '../dto/create-appointment.dto';
 import { CreateAppointmentService } from '../services/create-appointment.service';
+import { ListProvidersService } from '../services/list-providers.service';
 
 @Controller('appointments')
+@UseGuards(JwtAuthGuard)
 export class AppointmentsController {
   constructor(
     private readonly createAppointmentService: CreateAppointmentService,
+    private readonly listProvidersService: ListProvidersService,
   ) {}
 
-  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Req() req, @Body() { date, provider_id }: CreateAppointmentDTO) {
     return this.createAppointmentService.execute({
@@ -17,5 +19,10 @@ export class AppointmentsController {
       provider_id,
       user_id: req.user.id,
     });
+  }
+
+  @Get('me')
+  me(@Req() req) {
+    return this.listProvidersService.execute({ user_id: req.user.id });
   }
 }
