c3047cfad6a11796c675bcb6037735e95bc29776
Saymon Damásio <saymon.damasio95@gmail.com>
2022-02-07 16:59:42 -0300

    refactor: env variables

--- ormconfig.js
+++ ormconfig.js
@@ -5,7 +5,7 @@ const config = [
     name: 'default',
     type: 'postgres',
     host: process.env.DB_HOST,
-    port: 5432,
+    port: process.env.DB_PORT,
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
